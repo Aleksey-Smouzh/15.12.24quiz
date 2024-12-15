ce8ac6580920c24942ff49b88a3ae5eb3f576951
Aleksey-Smouzh <xmz@tut.by>
2024-12-15 12:42:00 +0100

    structura

---
+++ pytania.js
@@ -0,0 +1,37 @@
+const pytania = [
+    {
+        pytania: "co to jest HTML?",
+        odpowiedzi: ["Hypertext markup language",
+            "Hypertext markup languages",
+            "Hypertext markuptt languages",
+
+        ],
+        poprawna: 0,
+    },
+    {
+        pytania: "co to jest css?",
+        odpowiedzi: ["Hypertext markup language",
+            "Hypertext markup blblblbl",
+            "Hypertextbldlvld markuptt languages",
+
+        ],
+        poprawna: 2,
+    },
+    {
+        pytania: "co to jest JS?",
+        odpowiedzi: ["Hypertextbldlmdldld markup language",
+            "Hypertext markuppdpcbldlbl languages",
+            "Hypertextddlks markuptt languages",
+            "Hypertextddlks marfffhhkuptt languagesblbldlv",
+        ],
+        poprawna: 1,
+    },
+
+
+
+
+
+
+
+
+]
